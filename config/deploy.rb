e0fbcf684ccb8a23e69afdca7a85383eac823b73
Vitaly Shlyaga <storkvist@storkvist.net>
2015-11-30 12:33:12 +0300

    Деплой.

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,12 +28,13 @@ set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 set :passenger_restart_with_touch, true
 
