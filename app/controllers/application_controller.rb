344bc678b8e55d049ef0d236bb157bf5313a6368
Vitaly Shlyaga <storkvist@storkvist.net>
2015-12-10 14:30:40 +0300

    Вынес поля для редактирования Personable в отдельные шаблоны.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,15 @@ class ApplicationController < ActionController::Base
 
   rescue_from CanCan::AccessDenied, with: :access_denied
 
+  before_filter :append_view_paths
+
   private
 
   def access_denied
     return if redirect_to root_path, alert: 'Доступ запрещён.'
+  end
+
+  def append_view_paths
+    append_view_path 'app/views/shared'
   end
 end
--- app/helpers/nested_form_helper.rb
+++ app/helpers/nested_form_helper.rb
@@ -1,10 +1,12 @@
 # Функции для работы с вложенными формами (редактирование связанных моделей).
 module NestedFormHelper
-  def link_to_add_fields(name, f, association)
+  def link_to_add_fields(name, f, association, options = {})
+    options[:partial] ||= association.to_s.singularize + '_fields'
+
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object,
                           child_index: "new_#{association}") do |builder|
-      render(association.to_s.singularize + '_fields', f: builder)
+      render(options[:partial], f: builder)
     end
 
     link_to name, "##{association}",
