26bef85122ec50ce11f72eb6f308667d4b47c3d0
Anna Voronkina <vau2007@rambler.ru>
2015-12-10 12:46:40 +0300

    Rubocop

--- app/models/study/plan.rb
+++ app/models/study/plan.rb
@@ -1,6 +1,5 @@
 # Элементы учебного плана отдельного профиля.
 class Study::Plan < ActiveRecord::Base
-
   has_many :classes, class_name: 'Study::Class'
   has_many :zets, class_name: 'Study::Zet'
   has_many :exams, class_name: 'Study::Exam'
@@ -11,5 +10,4 @@ class Study::Plan < ActiveRecord::Base
 
   scope :from_admission_year, -> (year) { where(admission_year: year) }
   scope :from_part_id, -> (part_id) { where(part_id: part_id) }
-
 end
