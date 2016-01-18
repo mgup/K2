1b400193d0f8e10b6f697e35fad5a194e9b5fd3d
Vitaly Shlyaga <storkvist@storkvist.net>
2016-01-18 14:48:30 +0300

    Обновление до Foundation 6.

--- app/models/office/document_type.rb
+++ app/models/office/document_type.rb
@@ -8,4 +8,9 @@ class Office::DocumentType < ActiveRecord::Base
     where(prefix: %w(01-03 01-03р 01-04 01-05 01-06 01-17 \
                      01-26 01-26р 01-36 01-37 01-38))
   }
+
+  scope :for_incoming_documents, lambda {
+    where(prefix: %w(01-10 01-11 01-12 01-13 01-14 \
+                     01-15 01-18 01-19 01-20))
+  }
 end
