4405e96c73192b97ad198072987c7430b5236eb3
Vitaly Shlyaga <storkvist@storkvist.net>
2016-04-25 16:19:11 +0300

    Исправление для фабрики.

--- spec/factories/office/incoming_documents.rb
+++ spec/factories/office/incoming_documents.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
-  factory :office_incoming_document do
-    number 1
+  factory :office_incoming_document, class: 'Office::IncomingDocument' do
+    number { Faker::Number.number(3) }
+    date { Faker::Date.between(1.year.ago, Time.zone.today) }
+    title { Faker::Lorem.sentence }
+
+    association :document_type, factory: :office_document_type
   end
 end
